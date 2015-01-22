23f91a0c434300565f2c7141b719dfcf12e4327d
amalfi <mberendt546@gmail.com>
2015-01-22 22:03:27 +0100

    Getting data from lastFm api
    Added

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -47,4 +47,30 @@ angular.module('todoController', [])
 					$scope.todos = data; // assign our new list of todos
 				});
 		};
+		
+		$scope.importAlbumsFromLastFM = function(artistName)
+		{
+			  var albums = new Array();
+			  var artistName = $scope.formData.artist;
+			 // $scope.submitLastFm = function() 
+			 // {
+				 // if ($scope.text) 
+				 // {
+					
+					var lastFMApiRequestLink = "http://ws.audioscrobbler.com/2.0/?method=artist.getTopAlbums&artist="+artistName+
+					"&api_key=83d674fdb0f2798509110d0ed6261672&format=JSON";
+					$http.get(lastFMApiRequestLink).success(function(data) 
+					{
+									var albumsObjects = data.topalbums.album;
+									for(var i=0; i<albumsObjects.length; i++)
+									{
+										albums[i]=albumsObjects[i].name
+									}
+								//data.topalbums.album[1].name ('Master of Puppets');
+					})			
+				 // }
+			//  }
+			  $scope.albums=albums;
+					
+		};
 	}]);
